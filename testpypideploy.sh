a7d22e8c4dca70d79d5f7610c89679ee23c4f970
raulodev <raulodev@gmail.com>
2024-09-10 11:23:07 -0400

    create script to upload dis

---
+++ testpypideploy.sh
@@ -0,0 +1,16 @@
+# Requirements
+# python3 -m pip install --upgrade pip
+# python3 -m pip install --upgrade build
+# python3 -m pip install --upgrade twine
+
+# Build
+python3 -m build
+
+# Upload to testpypi
+python3 -m twine upload --repository testpypi dist/*
+
+# Upload to pypi
+# python3 -m twine upload dist/*
+
+# Install
+python3 -m pip install --index-url https://test.pypi.org/simple/  python-killbill-client
