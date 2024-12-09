8405c7f053c677ce55329cbc45a5f6e37d0a66be
raulodev <raulodev@gmail.com>
2024-12-09 08:18:09 -0500

    feat: add custom fields bundle

--- src/killbill/clients/bundle.py
+++ src/killbill/clients/bundle.py
@@ -1,9 +1,11 @@
-from killbill.clients.base import BaseClient
+from typing import List
+
+from killbill.clients.base import BaseClientWithCustomFields
+from killbill.enums import Audit, ObjectType
 from killbill.header import Header
-from killbill.enums import Audit
 
 
-class BundleClient(BaseClient):
+class BundleClient(BaseClientWithCustomFields):
 
     def list(
         self,
@@ -58,3 +60,60 @@ class BundleClient(BaseClient):
         self._raise_for_status(response)
 
         return response.json()
+
+    def add_custom_fields(
+        self,
+        header: Header,
+        bundle_id: str,
+        fields: dict,
+    ):
+        """Add custom fields to bundle"""
+
+        self._add_custom_fields(
+            header,
+            path="bundles",
+            object_id=bundle_id,
+            fields=fields,
+            object_type=ObjectType.BUNDLE,
+        )
+
+    def get_custom_fields(
+        self, header: Header, bundle_id: str, audit: Audit = Audit.NONE
+    ):
+        """Retrieve bunlde custom fields"""
+
+        return self._get_custom_fields(
+            header, path="bundles", object_id=bundle_id, audit=audit
+        )
+
+    def update_custom_fields(
+        self,
+        header: Header,
+        bundle_id: str,
+        fields: List[dict],
+    ):
+        """Modify custom fields to bundle
+
+        Example:
+        ```python
+        killbill.bundle.update_custom_fields(
+            header,
+            bundle_id="bundle_id",
+            fields=[
+                {
+                    "name": "name",
+                    "value": "value",
+                    "field_id": "field_id",
+                }
+            ],
+        )
+
+        ```
+        """
+        self._update_custom_fields(
+            header,
+            path="bundles",
+            object_id=bundle_id,
+            fields=fields,
+            object_type=ObjectType.BUNDLE,
+        )
