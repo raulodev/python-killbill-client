69436b959a5a1c3775797d2c0788a3f784b66783
raulodev <raulodev@gmail.com>
2025-01-20 17:38:03 -0500

    update exceps

--- src/killbill/exceptions.py
+++ src/killbill/exceptions.py
@@ -6,20 +6,28 @@ class KillBillError(Exception):
 
 
 class UnknownError(KillBillError):
-    def __init__(self, msg: object = "Unknown Error") -> None:
+    def __init__(self, msg: object = None) -> None:
+        if msg is None:
+            msg = "Unknown Error"
         super().__init__(msg)
 
 
 class AuthError(Exception):
-    def __init__(self, msg: object = "Unauthorized") -> None:
+    def __init__(self, msg: object = None) -> None:
+        if msg is None:
+            msg = "Unauthorized"
         super().__init__(msg)
 
 
 class NotFoundError(Exception):
-    def __init__(self, msg: object = "Source Not Found") -> None:
+    def __init__(self, msg: object = None) -> None:
+        if msg is None:
+            msg = "Source Not Found"
         super().__init__(msg)
 
 
 class BadRequestError(Exception):
-    def __init__(self, msg: object = "Bad Request") -> None:
+    def __init__(self, msg: object = None) -> None:
+        if msg is None:
+            msg = "Bad Request"
         super().__init__(msg)
